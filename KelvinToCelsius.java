78abd8ac7fac00c2d56ca1e3cced6ea0fe52a6a1
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-25 21:45:45 +0530

    Add files via upload

---
+++ KelvinToCelsius.java
@@ -0,0 +1,23 @@
+//Write a program in JAVA to convert temperature in Kelvin to Celsius. 
+//Sample Output:
+//Convert temperature in Kelvin to Celsius :
+//---------------------------------------------------------------
+//Input the temperature in Kelvin : 300
+//The temperature in Kelvin : 300
+//The temperature in Celsius : 26.85
+
+import java.util.Scanner;
+class KelvinToCelsius {
+	public static void main(String args[]) {
+		Double cel;
+		int kel;
+		System.out.println("Convert temperature in Kelvin to celsius:");
+		System.out.println("-------------------------------------");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input the temperature in Kelvin:");
+		kel = sc.nextInt();
+		cel = (kel - 273.15);
+		System.out.println("The temperature in kelvin:" + kel);
+		System.out.println("The temperature in celsius:" + cel);
+	}
+}

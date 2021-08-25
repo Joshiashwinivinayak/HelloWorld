0896b1ba336bcd35b7a6cb127ba9e4b9327c458c
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-25 14:19:36 +0530

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,15 @@
+//Write a program in JAVA to print the result of the specified operations.
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		System.out.print("Result of 1st expression is:");
+		System.out.println(3 + 5 * 4);
+		System.out.print("Result of 2nd expression is :");
+		System.out.println((55 + 13) % 9);
+		System.out.print("Result of 3rd expression is:");
+		System.out.println(7 + -3 * 5 / 3);
+		System.out.print("Result of 4th expression is:");
+		System.out.println(5 + 15 / 3 * 2 - 12);
+	}
+}

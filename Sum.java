ee2c010cb39b14e72375ef772cd28fe1e9e6de19
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-25 19:50:19 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,8 @@
+//Write a program in JAVA to print the sum of two numbers.                        Sample Output:                                        Print the sum of two numbers :                     -------------------------------------------------------------------The sum of 29 and 30 is : 59                          
+public class Sum {
+	public static void main(String[] args) {
+		System.out.println("Print the sum of two numbers:");
+		System.out.println("-------------------------------------");
+		System.out.println("sum of 20 and 30 is:" + (20 + 30));
+	}
+}

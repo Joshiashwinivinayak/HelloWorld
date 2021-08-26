b4724a0ac4662090914440b977bff55e3b942929
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-26 10:07:32 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,15 @@
+//Write a program in JAVA to divide two numbers and print on the screen.
+//Sample Output:
+//Divide two numbers and print:
+//---------------------------------------------------------------
+//The quotient of 30 and 10 is : 3
+
+public class Division {
+	public static void main(String args[]) {
+		System.out.println("Divide two numbers and print:");
+		System.out.println("-------------------------------------");
+		int num1 = 40, num2 = 4;
+		int result = num1 / num2;
+		System.out.println("Division of " + num1 + " and " + num2 + " is:" + result);
+	}
+}

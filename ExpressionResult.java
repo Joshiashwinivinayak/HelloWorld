eb4caa1386b2ad3f079eccec08265ec0bb0b3908
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-26 10:18:10 +0530

    Add files via upload

---
+++ ExpressionResult.java
@@ -0,0 +1,10 @@
+//Write a program in JAVA to compute the specified expressions and print the output.
+
+public class ExpressionResult {
+ public static void main(String[] arg) {
+ 	System.out.println("Compute the specified expressions and print the output:");
+ 	System.out.println("-------------------------------------");
+System.out.print("Result of Expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5):");
+        System.out.println( (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    }
+}

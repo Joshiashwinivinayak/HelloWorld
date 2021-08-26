42d4871eeab66d81662fec850f4353c35765d805
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-26 10:50:08 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,22 @@
+//Write a program in JAVA to print the following pattern.
+//Sample Output:
+//  xxxxx
+//x         x               x                x
+//x                          x                x
+//x                    xxxxxxx    xxxxxxx
+//x                          x                x
+//x          x              x                x
+//  xxxxx
+
+public class Pattern {
+	public static void main(String[] args) {
+		System.out.println(" xxxxx");
+		System.out.println("x     x       x        x");
+		System.out.println("x             x        x");
+		System.out.println("x          xxxxxxx  xxxxxxx");
+		System.out.println("x             x        x");
+		System.out.println("x     x       x        x");
+		System.out.println(" xxxxx\n");
+	}
+
+}

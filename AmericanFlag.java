34cb3765199e5475c8eaf1352f0d08a6d247b975
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-26 11:05:29 +0530

    Add files via upload

---
+++ AmericanFlag.java
@@ -0,0 +1,21 @@
+//Write a program in JAVA to print an American flag on the screen.
+
+public class AmericanFlag {
+	public static void main(String[] args) {
+		System.out.println("* * * * * * =========================");
+		System.out.println(" * * * * *  =========================");
+		System.out.println("* * * * * * =========================");
+		System.out.println(" * * * * *  =========================");
+		System.out.println("* * * * * * =========================");
+		System.out.println(" * * * * *  =========================");
+		System.out.println("* * * * * * =========================");
+		System.out.println(" * * * * *  =========================");
+		System.out.println("* * * * * * =========================");
+		System.out.println("=====================================");
+		System.out.println("=====================================");
+		System.out.println("=====================================");
+		System.out.println("=====================================");
+		System.out.println("=====================================");
+		System.out.println("=====================================");
+	}
+}

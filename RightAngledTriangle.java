093c1305a32d9f1ee50daff470366a75c066546b
Joshiashwinivinayak <89513242+Joshiashwinivinayak@users.noreply.github.com>
2021-08-27 13:30:10 +0530

    Add files via upload

---
+++ RightAngledTriangle.java
@@ -0,0 +1,27 @@
+//Write a JAVA program to check whether given length of three side form a right triangle.
+
+import java.util.Arrays;
+ import java.util.Comparator; 
+import java.util.Scanner; 
+class RightAngledTriangle { 
+Scanner sc = new Scanner(System.in); 
+public void run() { 		System.out.println("Input three integers(sides of a triangle)");
+ int[] int_num = new int[]{ 
+sc.nextInt(),sc.nextInt(),sc.nextInt() }; 
+Arrays.sort(int_num); 			System.out.println("If the given sides form a right triangle?"); ln((int_num[2]*int_num[2]==int_num[0]*int_num[0]+int_num[1]*int_num[1])?"Yes":"No"); 
+} 
+public static void main(String[] args) { 
+new Main().run(); 
+} 
+public static void pr(Object o) 
+{ 
+System.out.print(o); 
+} 
+public static void ln(Object o) { 
+System.out.println(o); 
+} 
+public static void ln()
+{ 
+System.out.println(); 
+} 
+}
